1f9edf1c666e79e6024dc04ed3cf3b0da76c3a5c
Rin Kates <erinkates11@gmail.com>
2023-04-01 15:24:37 -0400

    components added

--- reactmoveis/src/App.js
+++ reactmoveis/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+
+
+// WE IMPORT OUR COMPONENTS
+import MovieDisplay from "./components/MovieDisplay";
+import Form from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-
+      <Form />
+      <MovieDisplay />
     </div>
   );
 }
