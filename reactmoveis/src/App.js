1353522b09b1ac5959fb9a3b7b070639f9b7de89
Rin Kates <119820021+rin-11@users.noreply.github.com>
2023-05-07 21:33:29 -0400

    Update App.js

--- reactmoveis/src/App.js
+++ reactmoveis/src/App.js
@@ -1,32 +1,26 @@
 import React from "react";
 import "./App.css";
 
-// WE IMPORT OUR COMPONENTS
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 
 function App() {
-  //variable with your apiKey
   const apiKey = "46882cef";
 
 
-  //State to hold movie data
   const [movie, setMovie] = React.useState(null);
 
-  //Function to getMovies
+
   const getMovie = async (searchTerm) => {
-    // make fetch request and store response
     const response = await fetch(
       `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
     );
-    // Parse JSON response into a javascript object
     const data = await response.json();
     //set the Movie state to the movie
     setMovie(data);
   };
 
-  // USE OUR COMPONENTS IN APPs RETURNED JSX
-  // We pass the getMovie function as a prop called moviesearch
+
   return (
     <div className="App">
       <Form moviesearch={getMovie} />
