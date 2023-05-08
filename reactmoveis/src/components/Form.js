7ca01363f460aa0b9a773359a5af43773e9d68a0
Rin Kates <119820021+rin-11@users.noreply.github.com>
2023-05-07 21:32:55 -0400

    Update Form.js

--- reactmoveis/src/components/Form.js
+++ reactmoveis/src/components/Form.js
@@ -1,27 +1,21 @@
 import React from "react";
 
-// Define a function that is our component, always make sure to declare the props parameter so you can use props in your component
+
 const Form = (props) => {
-  //State to hold the data of our form
   const [formData, setFormData] = React.useState({
     searchterm: "",
   });
 
-  //handleChange - updates formData when we type into form
   const handleChange = (event) => {
-    //use the event object to detect key and value to update
     setFormData({ ...formData, [event.target.name]: event.target.value });
     console.log(formData)
   };
 
   const handleSubmit = (event) => {
-    //prevent page from refreshing on form submission
     event.preventDefault();
-    //pass the search term to moviesearch prop, which is apps getMovie function
     props.moviesearch(formData.searchterm);
   };
 
-  //The component must return some JSX
   return (
     <div>
         <h1>MOVIE FINDER</h1>
@@ -37,5 +31,4 @@ const Form = (props) => {
   );
 };
 
-// We must export the component to use it in other files
 export default Form;
